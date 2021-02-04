ffdd6b7b6e73d90facfbd190cdb26d5c58577b81
scart97 <scart.lucas@gmail.com>
2021-02-04 03:46:19 -0300

    Add model tests

---
+++ tests/test_jasper_model.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from urllib.error import HTTPError
+
+from torch import nn
+
+from thunder.jasper.model import get_jasper, read_config
+
+
+def test_can_open_quartznet():
+    # Quartznet 5x5 is small (25mb), so it can be downloaded while testing.
+    try:
+        with TemporaryDirectory() as tmpdir:
+            encoder, decoder = get_jasper("QuartzNet5x5LS-En", tmpdir)
+            assert isinstance(encoder, nn.Module)
+            assert isinstance(decoder, nn.Module)
+    except HTTPError:
+        return
+
+
+def test_create_from_manifest():
+    path = Path("tests/nemo_config_samples")
+    for cfg in path.glob("*.yaml"):
+        encoder, decoder = read_config(cfg)
+        assert isinstance(encoder, nn.Module)
+        assert isinstance(decoder, nn.Module)
