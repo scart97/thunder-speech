5ed81d7d609bfc10cc98c0bdf6000c7ee163bab4
scart97 <scart.lucas@gmail.com>
2021-03-15 18:53:44 -0300

    Add vocabulary class

---
+++ src/thunder/text_processing/vocab.py
@@ -0,0 +1,94 @@
+from typing import List
+
+
+class Vocab:
+    def __init__(
+        self,
+        initial_vocab_tokens: List[str],
+        pad_token: str = "<pad>",
+        unknown_token: str = "<unk>",
+        start_token: str = "<bos>",
+        end_token: str = "<eos>",
+    ):
+        """Class that represents a vocabulary, with the related methods
+        to numericalize a sequence of tokens into numbers, and do the
+        reverse mapping of numbers back to tokens.
+
+        Args:
+            initial_vocab_tokens : Basic list of tokens that will be part of the vocabulary. DO NOT INCLUDE SPECIAL TOKENS THERE. Even the blank is automatically added by the class.
+            pad_token : Token that will represent padding.
+            unknown_token : Token that will represent unknown elements. Notice that this is different than the blank used by ctc.
+            start_token : Token that will represent the beginning of the sequence.
+            end_token : Token that will represent the end of the sequence.
+        """
+        self.pad_token = pad_token
+        self.unknown_token = unknown_token
+        self.start_token = start_token
+        self.end_token = end_token
+
+        self.itos = [
+            pad_token,
+            unknown_token,
+            start_token,
+            end_token,
+        ] + initial_vocab_tokens
+        self.stoi = {token: i for i, token in enumerate(self.itos)}
+
+    def numericalize(self, tokens: List[str]) -> List[int]:
+        """Function to transform a list of tokens into the corresponding numeric representation.
+
+        Args:
+            tokens : A single list of tokens to be transformed
+
+        Returns:
+            The corresponding numeric representation
+        """
+        return [self.stoi.get(it, self.unknown_idx) for it in tokens]
+
+    def decode_into_text(self, indices: List[int]) -> List[str]:
+        """Function to transform back a list of numbers into the corresponding
+        tokens.
+
+        Args:
+            indices : Numeric representation. Usually is the result of the model, after a greedy decoding
+
+        Returns:
+            Corresponding tokens
+        """
+        return [self.itos[it] for it in indices]
+
+    def add_special_tokens(self, tokens: List[str]) -> List[str]:
+        """Function to add the special start and end tokens to some
+        tokenized text.
+
+        Args:
+            tokens : Tokenized text
+
+        Returns:
+            Text with the special tokens added.
+        """
+        return [self.start_token] + tokens + [self.end_token]
+
+    @property
+    def unknown_idx(self):
+        return self.itos.index(self.unknown_token)
+
+    @property
+    def start_idx(self):
+        return self.itos.index(self.start_token)
+
+    @property
+    def end_idx(self):
+        return self.itos.index(self.end_token)
+
+    @property
+    def pad_idx(self):
+        return self.itos.index(self.pad_token)
+
+    @property
+    def blank_token(self):
+        return self.pad_token
+
+    @property
+    def blank_idx(self):
+        return self.pad_idx
---
+++ tests/test_text_vocab.py
@@ -0,0 +1,53 @@
+from string import ascii_lowercase
+
+import pytest
+
+from thunder.text_processing.vocab import Vocab
+
+
+@pytest.fixture
+def simple_vocab():
+    return Vocab(initial_vocab_tokens=[" "] + list(ascii_lowercase))
+
+
+def test_vocab_mapping_is_bidirectionally_correct(simple_vocab: Vocab):
+    assert len(simple_vocab.itos) == len(simple_vocab.stoi)
+    for k, v in simple_vocab.stoi.items():
+        assert simple_vocab.itos[v] == k
+
+
+def test_vocab_blank_is_not_the_unknown(simple_vocab: Vocab):
+    assert simple_vocab.blank_idx != simple_vocab.unknown_idx
+    assert simple_vocab.blank_token != simple_vocab.unknown_token
+
+
+def test_numericalize_adds_unknown_token(simple_vocab: Vocab):
+    out = simple_vocab.numericalize(["a", "b", "c", "$"])
+    assert out == [5, 6, 7, simple_vocab.unknown_idx]
+
+
+def test_numericalize_decode_is_bidirectionally_correct(simple_vocab: Vocab):
+    inp = ["a", "b", "c", "d", "e"]
+    out1 = simple_vocab.numericalize(inp)
+    out = simple_vocab.decode_into_text(out1)
+    assert out == inp
+
+
+def test_add_special_tokens(simple_vocab: Vocab):
+    inp = ["a", "b", "c"]
+    out = simple_vocab.add_special_tokens(inp)
+    assert out == [simple_vocab.start_token, "a", "b", "c", simple_vocab.end_token]
+
+
+def test_special_idx_are_different(simple_vocab: Vocab):
+    all_tokens = set(
+        [
+            simple_vocab.start_idx,
+            simple_vocab.end_idx,
+            simple_vocab.pad_idx,
+            simple_vocab.unknown_idx,
+            simple_vocab.blank_idx,
+        ]
+    )
+    # There's no problem if the blank_idx == pad_idx
+    assert len(all_tokens) >= 4
