73e11362d8fc7babd22da6dc34baa94e8902ac3e
scart97 <scart.lucas@gmail.com>
2021-07-02 20:05:34 -0300

    feat(citrinet): update module to use dataclass config

--- src/thunder/citrinet/module.py
+++ src/thunder/citrinet/module.py
@@ -7,11 +7,19 @@ to enable easy finetuning and inference.
 
 # Copyright (c) 2021 scart97
 
-__all__ = ["CitrinetModule", "CitrinetCheckpoint"]
+__all__ = [
+    "CitrinetModule",
+    "CitrinetCheckpoint",
+    "TextTransformConfig",
+    "EncoderConfig",
+    "FilterbankConfig",
+    "OptimizerConfig",
+]
 
+from dataclasses import dataclass
 from pathlib import Path
 from tempfile import TemporaryDirectory
-from typing import List, Optional, Tuple
+from typing import List, Tuple
 
 import pytorch_lightning as pl
 import torch
@@ -19,7 +27,7 @@ from torch import Tensor
 from torchaudio.datasets.utils import extract_archive
 
 from thunder.blocks import conv1d_decoder
-from thunder.citrinet.blocks import Citrinet_encoder
+from thunder.citrinet.blocks import Citrinet_encoder, EncoderConfig
 from thunder.citrinet.compatibility import (
     CitrinetCheckpoint,
     fix_vocab,
@@ -28,72 +36,49 @@ from thunder.citrinet.compatibility import (
 from thunder.ctc_loss import calculate_ctc
 from thunder.metrics import CER, WER
 from thunder.quartznet.compatibility import load_quartznet_weights
-from thunder.quartznet.transform import FilterbankFeatures
-from thunder.text_processing.transform import BatchTextTransformer
+from thunder.quartznet.transform import FilterbankConfig, FilterbankFeatures
+from thunder.text_processing.transform import BatchTextTransformer, TextTransformConfig
 from thunder.utils import download_checkpoint
+
+
+@dataclass
+class OptimizerConfig:
+    learning_rate: float = 3e-4
+    betas: Tuple[float] = (0.8, 0.5)
 
 
 class CitrinetModule(pl.LightningModule):
     def __init__(
         self,
-        initial_vocab_tokens: List[str],
-        filters: List[int],
-        kernel_sizes: List[int],
-        strides: List[int],
-        sentencepiece_model: Optional[str] = None,
-        sample_rate: int = 16000,
-        n_window_size: int = 320,
-        n_window_stride: int = 160,
-        n_fft: int = 512,
-        preemph: float = 0.97,
-        nfilt: int = 64,
-        dither: float = 1e-5,
-        learning_rate: float = 3e-4,
-        nemo_compat_vocab: bool = False,
+        text_cfg: TextTransformConfig,
+        encoder_cfg: EncoderConfig,
+        audio_cfg: FilterbankConfig = FilterbankConfig(),
+        optim_cfg: OptimizerConfig = OptimizerConfig(),
     ):
         """Module containing both the basic quartznet model and helper functionality, such as
         feature creation and text processing.
 
         Args:
-            initial_vocab_tokens : List of tokens to be used in the vocab, special tokens should not be included here. Check [`docs`](https://scart97.github.io/thunder-speech/quick%20reference%20guide/#how-to-get-the-initial_vocab_tokens-from-my-dataset)
-            filters : Check [`Citrinet_encoder`][thunder.citrinet.blocks.Citrinet_encoder]
-            kernel_sizes : Check [`Citrinet_encoder`][thunder.citrinet.blocks.Citrinet_encoder]
-            strides : Check [`Citrinet_encoder`][thunder.citrinet.blocks.Citrinet_encoder]
-            sample_rate : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            n_window_size : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            n_window_stride : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            n_fft : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            preemph : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            nfilt : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            dither : Check [`FilterbankFeatures`][thunder.quartznet.transform.FilterbankFeatures]
-            learning_rate : Learning rate used by the optimizer
-            nemo_compat_vocab : Controls if the used vocabulary will be compatible with the original nemo implementation.
+            audio_cfg: Configuration for the filterbank features applied to the input audio
+            encoder_cfg: Configuration for the quartznet encoder
+            text_cfg: Configuration for the text processing pipeline
+            optim_cfg: Configuration for the optimizer used during training
         """
         super().__init__()
         self.save_hyperparameters()
 
-        self.audio_transform = FilterbankFeatures(
-            sample_rate=sample_rate,
-            n_window_size=n_window_size,
-            n_window_stride=n_window_stride,
-            n_fft=n_fft,
-            preemph=preemph,
-            nfilt=nfilt,
-            dither=dither,
-        )
+        self.audio_transform = FilterbankFeatures(audio_cfg)
 
-        self.encoder = Citrinet_encoder(nfilt, filters, kernel_sizes, strides)
+        self.encoder = Citrinet_encoder(encoder_cfg)
 
-        self.text_transform = BatchTextTransformer(
-            initial_vocab_tokens, nemo_compat_vocab, sentencepiece_model
-        )
+        self.text_transform = BatchTextTransformer(text_cfg)
         self.decoder = conv1d_decoder(640, num_classes=len(self.text_transform.vocab))
 
         # Metrics
         self.val_cer = CER()
         self.val_wer = WER()
         # Example input is one second of fake audio
-        self.example_input_array = torch.randn((10, sample_rate))
+        self.example_input_array = torch.randn((10, audio_cfg.sample_rate))
 
     def forward(self, x: Tensor) -> Tensor:
         """Process the audio tensor to create the predictions.
@@ -182,8 +167,8 @@ class CitrinetModule(pl.LightningModule):
         """
         return torch.optim.Adam(
             filter(lambda p: p.requires_grad, self.parameters()),
-            lr=self.hparams.learning_rate,
-            betas=(0.8, 0.5),
+            lr=self.hparams.optim_cfg.learning_rate,
+            betas=self.hparams.optim_cfg.betas,
         )
 
     @classmethod
@@ -220,11 +205,13 @@ class CitrinetModule(pl.LightningModule):
             sentencepiece_path = str(extract_path / "tokenizer.model")
 
             module = cls(
-                initial_vocab_tokens=fix_vocab(initial_vocab),
-                sentencepiece_model=sentencepiece_path,
-                **encoder_params,
-                **preprocess_params,
-                nemo_compat_vocab=True,
+                TextTransformConfig(
+                    initial_vocab_tokens=fix_vocab(initial_vocab),
+                    sentencepiece_model=sentencepiece_path,
+                    simple_vocab=True,
+                ),
+                EncoderConfig(**encoder_params),
+                FilterbankConfig(**preprocess_params),
             )
             weights_path = extract_path / "model_weights.ckpt"
             load_quartznet_weights(module.encoder, module.decoder, str(weights_path))
@@ -235,12 +222,7 @@ class CitrinetModule(pl.LightningModule):
         module.eval()
         return module
 
-    def change_vocab(
-        self,
-        new_vocab_tokens: List[str],
-        sentencepiece_model: Optional[str] = None,
-        nemo_compat: bool = False,
-    ):
+    def change_vocab(self, text_cfg: TextTransformConfig):
         """Changes the vocabulary of the model. useful when finetuning to another language.
 
         Args:
@@ -249,11 +231,6 @@ class CitrinetModule(pl.LightningModule):
             nemo_compat : Controls if the used vocabulary will be compatible with the original nemo implementation.
         """
         # Updating hparams so that the saved model can be correctly loaded
-        self.hparams.initial_vocab_tokens = new_vocab_tokens
-        self.hparams.nemo_compat_vocab = nemo_compat
-        self.hparams.sentencepiece_model = sentencepiece_model
-
-        self.text_transform = BatchTextTransformer(
-            new_vocab_tokens, nemo_compat, sentencepiece_model
-        )
+        self.hparams.text_cfg = text_cfg
+        self.text_transform = BatchTextTransformer(text_cfg)
         self.decoder = conv1d_decoder(640, num_classes=len(self.text_transform.vocab))
