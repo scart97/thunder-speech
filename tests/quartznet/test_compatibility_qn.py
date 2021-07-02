205706ec512c610eefea4067c3d863006cd85da9
scart97 <scart.lucas@gmail.com>
2021-07-02 18:36:26 -0300

    test(quartznet): update compatibility test to use the new configs

--- tests/quartznet/test_compatibility_qn.py
+++ tests/quartznet/test_compatibility_qn.py
@@ -12,13 +12,13 @@ from torchaudio.datasets.utils import extract_archive
 
 from tests.utils import mark_slow
 from thunder.blocks import conv1d_decoder
-from thunder.quartznet.blocks import Quartznet_encoder
+from thunder.quartznet.blocks import EncoderConfig, Quartznet_encoder
 from thunder.quartznet.compatibility import (
     NemoCheckpoint,
     load_quartznet_weights,
     read_params_from_config,
 )
-from thunder.quartznet.transform import FilterbankFeatures
+from thunder.quartznet.transform import FilterbankConfig, FilterbankFeatures
 from thunder.utils import download_checkpoint
 
 
@@ -33,7 +33,7 @@ def test_can_load_weights():
             extract_archive(str(cfg), extract_path)
             config_path = extract_path / "model_config.yaml"
             encoder_params, initial_vocab, _ = read_params_from_config(config_path)
-            encoder = Quartznet_encoder(64, **encoder_params)
+            encoder = Quartznet_encoder(EncoderConfig(**encoder_params))
             decoder = conv1d_decoder(1024, len(initial_vocab) + 1)
             load_quartznet_weights(
                 encoder, decoder, extract_path / "model_weights.ckpt"
@@ -47,8 +47,8 @@ def test_create_from_manifest():
     path = Path("tests/nemo_config_samples")
     for cfg in path.glob("*.yaml"):
         encoder_params, initial_vocab, preprocess_params = read_params_from_config(cfg)
-        fb = FilterbankFeatures(**preprocess_params)
-        encoder = Quartznet_encoder(64, **encoder_params)
+        fb = FilterbankFeatures(FilterbankConfig(**preprocess_params))
+        encoder = Quartznet_encoder(EncoderConfig(**encoder_params))
         decoder = conv1d_decoder(1024, len(initial_vocab))
 
         x = torch.randn(10, 1337)
@@ -63,8 +63,8 @@ def test_create_from_manifest():
         assert not torch.isnan(out2).any()
 
         if "Net5x5" in cfg.name:
-            encoder2 = Quartznet_encoder(64)
+            encoder2 = Quartznet_encoder()
             encoder2.load_state_dict(encoder.state_dict())
         else:
-            encoder2 = Quartznet_encoder(64, repeat_blocks=3)
+            encoder2 = Quartznet_encoder(EncoderConfig(repeat_blocks=3))
             encoder2.load_state_dict(encoder.state_dict())
