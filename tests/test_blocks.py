639e543ff2f7763ca507eae1f59ba43ef0dd4407
scart97 <scart.lucas@gmail.com>
2021-07-21 20:16:44 -0300

    test(blocks): add fourier matrix and conv_stft GPU tests

--- tests/test_blocks.py
+++ tests/test_blocks.py
@@ -1,6 +1,14 @@
 import torch
+from pytorch_lightning import seed_everything
 
-from thunder.blocks import convolution_stft
+from thunder.blocks import _fourier_matrix, convolution_stft
+
+
+def test_fourier_transform_matrix():
+    for n in [64, 128, 256, 512, 1024]:
+        mat1 = torch.fft.fft(torch.eye(n))
+        mat2 = _fourier_matrix(n, "cpu")
+        assert torch.allclose(mat1, mat2, atol=1e-3)
 
 
 def test_convolution_stft():
@@ -13,4 +21,26 @@ def test_convolution_stft():
     out_original = torch.stft(
         x, n_fft=1024, hop_length=512, win_length=256, window=window_tensor
     )
-    assert torch.allclose(stft, out_original, atol=1e-3)
+    assert torch.allclose(stft, out_original, atol=1e-2)
+
+
+def test_convolution_stft_device_move():
+    x = torch.randn(10, 1000)
+    window_tensor = torch.hann_window(256, periodic=False)
+
+    def apply_op(inp):
+        return convolution_stft(
+            inp,
+            n_fft=1024,
+            hop_length=512,
+            win_length=256,
+            window=window_tensor,
+        )
+
+    seed_everything(42)
+    outputs_cpu = apply_op(x)
+
+    seed_everything(42)
+    outputs_gpu = apply_op(x.cuda())
+
+    assert torch.allclose(outputs_cpu, outputs_gpu.cpu(), atol=1e-3)
