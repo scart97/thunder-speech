d824e366724b7b47583f78b635b70f198bddb6d9
scart97 <scart.lucas@gmail.com>
2021-09-28 19:29:18 -0300

    refactor(text_processing): remove BatchTextTransformer config

--- src/thunder/text_processing/transform.py
+++ src/thunder/text_processing/transform.py
@@ -3,9 +3,8 @@
 
 # Copyright (c) 2021 scart97
 
-__all__ = ["BatchTextTransformer", "TextTransformConfig"]
+__all__ = ["BatchTextTransformer"]
 
-from dataclasses import dataclass
 from typing import List, Optional
 
 import torch
@@ -16,67 +15,29 @@ from thunder.text_processing.tokenizer import BPETokenizer, char_tokenizer
 from thunder.text_processing.vocab import SimpleVocab, Vocab
 
 
-@dataclass
-class TextTransformConfig:
-    """Configuration to create [`BatchTextTransformer`][thunder.text_processing.transform.BatchTextTransformer]
-
-    Attributes:
-        initial_vocab_tokens: List of tokens to create the vocabulary, special tokens should not be included here. required.
-        simple_vocab: Controls if the used vocabulary will only have the blank token or more additional special tokens. defaults to `False`.
-        sentencepiece_model: Path to sentencepiece .model file, if applicable.
-    """
-
-    initial_vocab_tokens: List[str]
-    simple_vocab: bool = False
-    sentencepiece_model: Optional[str] = None
-
-    @classmethod
-    def from_sentencepiece(cls, output_dir: str) -> "TextTransformConfig":
-        """Load the data from a folder that contains the `tokenizer.vocab`
-        and `tokenizer.model` outputs from sentencepiece.
-
-        Args:
-            output_dir : Output directory of the sentencepiece training, that contains the required files.
-
-        Returns:
-            Instance of `TextTransformConfig` with the corresponding data loaded.
-        """
-        special_tokens = ["<s>", "</s>", "<pad>", "<unk>"]
-        vocab = []
-
-        with open(f"{output_dir}/tokenizer.vocab", "r") as f:
-            # Read tokens from each line and parse for vocab
-            for line in f:
-                piece = line.split("\t")[0]
-                if piece in special_tokens:
-                    # skip special tokens
-                    continue
-                vocab.append(piece)
-
-        return cls(
-            initial_vocab_tokens=vocab,
-            sentencepiece_model=f"{output_dir}/tokenizer.model",
-        )
-
-
 class BatchTextTransformer(nn.Module):
-    def __init__(self, cfg: TextTransformConfig):
+    def __init__(
+        self,
+        initial_vocab_tokens: List[str],
+        simple_vocab: bool = False,
+        sentencepiece_model: Optional[str] = None,
+    ):
         """That class is the glue code that uses all of the text processing
         functions to encode/decode an entire batch of text at once.
 
         Args:
-            cfg: required config to create instance
+            initial_vocab_tokens: List of tokens to create the vocabulary, special tokens should not be included here. required.
+            simple_vocab: Controls if the used vocabulary will only have the blank token or more additional special tokens. defaults to `False`.
+            sentencepiece_model: Path to sentencepiece .model file, if applicable.
         """
         super().__init__()
         self.vocab = (
-            SimpleVocab(cfg.initial_vocab_tokens)
-            if cfg.simple_vocab
-            else Vocab(cfg.initial_vocab_tokens)
+            SimpleVocab(initial_vocab_tokens)
+            if simple_vocab
+            else Vocab(initial_vocab_tokens)
         )
         self.tokenizer = (
-            BPETokenizer(cfg.sentencepiece_model)
-            if cfg.sentencepiece_model
-            else char_tokenizer
+            BPETokenizer(sentencepiece_model) if sentencepiece_model else char_tokenizer
         )
 
     def encode(self, items: List[str], return_length: bool = True, device=None):
@@ -123,3 +84,31 @@ class BatchTextTransformer(nn.Module):
             out_list.append(out)
 
         return out_list
+
+    @classmethod
+    def from_sentencepiece(cls, output_dir: str) -> "BatchTextTransformer":
+        """Load the data from a folder that contains the `tokenizer.vocab`
+        and `tokenizer.model` outputs from sentencepiece.
+
+        Args:
+            output_dir : Output directory of the sentencepiece training, that contains the required files.
+
+        Returns:
+            Instance of `BatchTextTransformer` with the corresponding data loaded.
+        """
+        special_tokens = ["<s>", "</s>", "<pad>", "<unk>"]
+        vocab = []
+
+        with open(f"{output_dir}/tokenizer.vocab", "r") as f:
+            # Read tokens from each line and parse for vocab
+            for line in f:
+                piece = line.split("\t")[0]
+                if piece in special_tokens:
+                    # skip special tokens
+                    continue
+                vocab.append(piece)
+
+        return cls(
+            initial_vocab_tokens=vocab,
+            sentencepiece_model=f"{output_dir}/tokenizer.model",
+        )
