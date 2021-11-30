ac0521f4121a9e1d3cb936dcaf5b1b2c272a64fc
scart97 <scart.lucas@gmail.com>
2021-11-30 06:25:16 -0300

    feat(registry): provide generic load_checkpoint_data function

--- src/thunder/registry.py
+++ src/thunder/registry.py
@@ -1,3 +1,8 @@
+"""Functionality to register the multiple checkpoints and provide a unified loading interface.
+"""
+
+__all__ = ["register_checkpoint", "load_checkpoint_data"]
+
 from functools import partial
 from typing import Callable, Dict, Type
 
@@ -7,6 +12,7 @@ from thunder.quartznet.compatibility import (
     load_quartznet_checkpoint,
 )
 from thunder.utils import BaseCheckpoint, CheckpointResult
+from thunder.wav2vec.compatibility import load_huggingface_checkpoint
 
 CHECKPOINT_LOAD_FUNC_TYPE = Callable[..., CheckpointResult]
 
@@ -16,6 +22,12 @@ CHECKPOINT_REGISTRY: Dict[str, CHECKPOINT_LOAD_FUNC_TYPE] = {}
 def register_checkpoint(
     checkpoints: Type[BaseCheckpoint], load_function: CHECKPOINT_LOAD_FUNC_TYPE
 ):
+    """Register all variations of some checkpoint enum with the corresponding loading function
+
+    Args:
+        checkpoints : Base checkpoint class
+        load_function : function to load the checkpoint,
+            must receive one instance of `checkpoints` as first argument"""
     for checkpoint in checkpoints:
         CHECKPOINT_REGISTRY.update(
             {checkpoint.name: partial(load_function, checkpoint)}
@@ -24,3 +36,21 @@ def register_checkpoint(
 
 register_checkpoint(QuartznetCheckpoint, load_quartznet_checkpoint)
 register_checkpoint(CitrinetCheckpoint, load_citrinet_checkpoint)
+
+
+def load_checkpoint_data(checkpoint_name: str, **load_kwargs) -> CheckpointResult:
+    """Load data from any registered checkpoint
+
+    Args:
+        checkpoint_name : Base checkpoint name, like "QuartzNet5x5LS_En" or "facebook/wav2vec2-large-960h"
+
+    Returns:
+        Object containing the checkpoint data (encoder, decoder, transforms and additional data).
+    """
+    # Special case when dealing with any huggingface model
+    if "/" in checkpoint_name:
+        model_data = load_huggingface_checkpoint(checkpoint_name, **load_kwargs)
+    else:
+        load_fn = CHECKPOINT_REGISTRY[checkpoint_name]
+        model_data = load_fn(**load_kwargs)
+    return model_data
