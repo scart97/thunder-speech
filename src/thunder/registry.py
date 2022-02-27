0ca87e328c10c6cef5afb5a13e47c072e5122172
scart97 <scart.lucas@gmail.com>
2022-02-27 04:56:24 +0000

    feat(registry): load_pretrained now accepts both the checkpoint enum and string identifier

--- src/thunder/registry.py
+++ src/thunder/registry.py
@@ -9,7 +9,7 @@
 __all__ = ["register_checkpoint_enum", "load_pretrained"]
 
 from functools import partial
-from typing import Callable, Dict, Type
+from typing import Callable, Dict, Type, Union
 
 from thunder.citrinet.compatibility import CitrinetCheckpoint, load_citrinet_checkpoint
 from thunder.huggingface.compatibility import load_huggingface_checkpoint
@@ -44,7 +44,9 @@ register_checkpoint_enum(QuartznetCheckpoint, load_quartznet_checkpoint)
 register_checkpoint_enum(CitrinetCheckpoint, load_citrinet_checkpoint)
 
 
-def load_pretrained(checkpoint_name: str, **load_kwargs) -> BaseCTCModule:
+def load_pretrained(
+    checkpoint_name: Union[str, BaseCheckpoint], **load_kwargs
+) -> BaseCTCModule:
     """Load data from any registered checkpoint
 
     Args:
@@ -53,6 +55,8 @@ def load_pretrained(checkpoint_name: str, **load_kwargs) -> BaseCTCModule:
     Returns:
         Object containing the checkpoint data (encoder, decoder, transforms and additional data).
     """
+    if isinstance(checkpoint_name, BaseCheckpoint):
+        checkpoint_name = checkpoint_name.name
     # Special case when dealing with any huggingface model
     if "/" in checkpoint_name:
         model_data = load_huggingface_checkpoint(checkpoint_name, **load_kwargs)
