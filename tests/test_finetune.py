73c57fb12d7b09ad77afa41812fc00ad6bb3a417
scart97 <scart.lucas@gmail.com>
2022-04-16 16:32:13 +1000

    test(finetune): Update test to correcly use scheduler

--- tests/test_finetune.py
+++ tests/test_finetune.py
@@ -65,22 +65,9 @@ def test_special_scheduler_args(sample_manifest):
         num_workers=0,
     )
     trainer = pl.Trainer(
-        fast_dev_run=True, logger=False, enable_checkpointing=False, gpus=-1
+        max_epochs=3,
+        logger=False,
+        enable_checkpointing=False,
+        gpus=-1,
     )
     trainer.fit(module, datamodule=data)
-
-    # Alternative, passing epochs and steps_per_epoch as two arguments
-    # instead of total training steps
-    module2 = FinetuneCTCModule(
-        "stt_en_citrinet_256",
-        decoder_class=conv1d_decoder,
-        tokens=list(ascii_lowercase),
-        text_kwargs={"blank_token": "BLANK"},
-        lr_scheduler_class=OneCycleLR,
-        lr_scheduler_kwargs={
-            "max_lr": 1e-3,
-            "steps_arg": "steps_per_epoch",
-            "epochs_arg": "epochs",
-        },
-    )
-    trainer.fit(module2, datamodule=data)
