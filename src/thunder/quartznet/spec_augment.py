9d68225ec8242d83d487cbdc153cd5ec0cfe442c
scart97 <scart.lucas@gmail.com>
2022-07-12 09:27:46 +1000

    refactor(spec_augment): replace custom code with torchaudio functions

--- src/thunder/quartznet/spec_augment.py
+++ src/thunder/quartznet/spec_augment.py
@@ -14,10 +14,9 @@
 
 # Original file: https://github.com/NVIDIA/NeMo/blob/54e6f6ee688f09810d3e54661275fd5c8718db00/nemo/collections/asr/parts/spectr_augment.py
 
-import random
-
 import torch
 import torch.nn as nn
+from torchaudio.functional import mask_along_axis
 
 
 class SpecAugment(nn.Module):
@@ -31,11 +30,6 @@ class SpecAugment(nn.Module):
     time_masks - how many time segments should be cut
     freq_width - maximum number of frequencies to be cut in one segment
     time_width - maximum number of time steps to be cut in one segment.
-        Can be a positive integer or a float value in the range [0, 1].
-        If positive integer value, defines maximum number of time steps
-        to be cut in one segment.
-        If a float value, defines maximum percentage of timesteps that
-        are cut adaptively.
     """
 
     def __init__(
@@ -44,57 +38,40 @@ class SpecAugment(nn.Module):
         time_masks=0,
         freq_width=10,
         time_width=10,
-        rng=None,
-        mask_value=0.0,
     ):
         super().__init__()
-
-        self._rng = random.Random() if rng is None else rng
-
         self.freq_masks = freq_masks
         self.time_masks = time_masks
-
         self.freq_width = freq_width
         self.time_width = time_width
-
-        self.mask_value = mask_value
-
-        if isinstance(time_width, int):
-            self.adaptive_temporal_width = False
-        else:
-            if time_width > 1.0 or time_width < 0.0:
-                raise ValueError(
-                    "If `time_width` is a float value, must be in range [0, 1]"
-                )
-
-            self.adaptive_temporal_width = True
 
     @torch.no_grad()
     def forward(self, x):
         if self.training:
-            sh = x.shape
+            for _ in range(self.time_masks):
+                x = mask_along_axis(x, self.time_width, 0.0, 2)
 
-            if self.adaptive_temporal_width:
-                time_width = max(1, int(sh[2] * self.time_width))
-            else:
-                time_width = self.time_width
-
-            for idx in range(sh[0]):
-                for _ in range(self.freq_masks):
-                    x_left = self._rng.randint(0, sh[1] - self.freq_width)
-
-                    w = self._rng.randint(1, self.freq_width)
-
-                    x[idx, x_left : x_left + w, :] = self.mask_value
-
-                for _ in range(self.time_masks):
-                    y_left = self._rng.randint(0, sh[2] - time_width)
-
-                    w = self._rng.randint(1, time_width)
-
-                    x[idx, :, y_left : y_left + w] = self.mask_value
+            for _ in range(self.freq_masks):
+                x = mask_along_axis(x, self.freq_width, 0.0, 1)
 
         return x
+
+
+def _create_mask(specgram, mask_param, axis):
+    # modified from mask_along_axis in torchaudio
+    # source: https://github.com/pytorch/audio/blob/54eb0991fae635c6586f7f1d6bf6080128fbff11/torchaudio/functional/functional.py#L870
+    value = torch.rand(1) * mask_param
+    min_value = torch.rand(1) * (specgram.size(axis) - value)
+
+    mask_start = (min_value.long()).squeeze()
+    mask_end = (min_value.long() + value.long()).squeeze()
+    mask = torch.arange(
+        0, specgram.shape[axis], device=specgram.device, dtype=specgram.dtype
+    )
+    mask = (mask >= mask_start) & (mask < mask_end)
+    if axis == 1:
+        mask = mask.unsqueeze(-1)
+    return mask
 
 
 class SpecCutout(nn.Module):
@@ -104,31 +81,21 @@ class SpecCutout(nn.Module):
 
     params:
     rect_masks - how many rectangular masks should be cut
-    rect_freq - maximum size of cut rectangles along the frequency dimension
-    rect_time - maximum size of cut rectangles along the time dimension
+    freq_width - maximum size of cut rectangles along the frequency dimension
+    time_width - maximum size of cut rectangles along the time dimension
     """
 
-    def __init__(self, rect_masks=0, rect_time=5, rect_freq=20, rng=None):
+    def __init__(self, rect_masks=0, time_width=5, freq_width=20):
         super().__init__()
-
-        self._rng = random.Random() if rng is None else rng
-
         self.rect_masks = rect_masks
-        self.rect_time = rect_time
-        self.rect_freq = rect_freq
+        self.time_width = time_width
+        self.freq_width = freq_width
 
     @torch.no_grad()
     def forward(self, x):
         if self.training:
-            sh = x.shape
-
-            for idx in range(sh[0]):
-                for _ in range(self.rect_masks):
-                    rect_x = self._rng.randint(0, sh[1] - self.rect_freq)
-                    rect_y = self._rng.randint(0, sh[2] - self.rect_time)
-
-                    w_x = self._rng.randint(1, self.rect_time)
-                    w_y = self._rng.randint(1, self.rect_freq)
-                    x[idx, rect_x : rect_x + w_x, rect_y : rect_y + w_y] = 0.0
-
+            for _ in range(self.rect_masks):
+                freq_mask = _create_mask(x, self.freq_width, 1)
+                time_mask = _create_mask(x, self.freq_width, 2)
+                x = x.masked_fill(freq_mask & time_mask, 0.0)
         return x
