9af6e2789fcf1f99f52215c57876640bfafa68b6
scart97 <scart.lucas@gmail.com>
2024-10-15 00:31:05 -0300

    chore: removing old code

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@
 
 from pathlib import Path
 
-from thunder.utils import audio_len, chain_calls, get_default_cache_folder, get_files
+from thunder.utils import audio_len, get_default_cache_folder, get_files
 
 
 def test_audio_len(sample_data):
@@ -43,29 +43,3 @@ def test_get_files_dont_exist():
     manifest_files = get_files(path, ".mp3")
 
     assert len(manifest_files) == 0
-
-
-def test_chain_calls():
-    f1 = lambda x: 2 * x  # noqa: E731
-    f2 = lambda x: 3 * x  # noqa: E731
-    f3 = lambda x: 4 * x  # noqa: E731
-    g = chain_calls(f1, f2, f3)
-    assert g(1) == 24
-
-
-def test_chain_calls_dont_modify_input_alone():
-    g = chain_calls()
-    assert g([1, 2, 3]) == [1, 2, 3]
-    assert g(1) == 1
-    assert g("abc") == "abc"
-    assert g((1, 2, 3)) == (1, 2, 3)
-
-
-def test_chain_calls_respect_order():
-    f1 = lambda x: x + "a"  # noqa: E731
-    f2 = lambda x: x + "b"  # noqa: E731
-    f3 = lambda x: x + "c"  # noqa: E731
-    g = chain_calls(f1, f2, f3)
-    assert g("") == "abc"
-    g2 = chain_calls(f2, f1, f3)
-    assert g2("") == "bac"
